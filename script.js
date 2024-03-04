a6b2a295a0642d073ec6bfc7e2e1b36cf611a0a7
ferndz <nafernandez@mi.unc.edu.ar>
2024-03-04 17:53:51 -0300

    fixed: password validation

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
+const password = document.getElementById("password");
+
 const confirmPassword = document.getElementById("confirm-password");
 
 
 confirmPassword.addEventListener("input", (event) => {
-    const password = document.getElementById("password");
-    console.log(password.value, confirmPassword.value);
-    if(password.value === confirmPassword.value){
+    const currentPassword = document.getElementById("password");
+    if(currentPassword.value === confirmPassword.value){
         confirmPassword.setCustomValidity("");
     } else {
         confirmPassword.setCustomValidity("Passwords don't match");
+    }
+});
+
+password.addEventListener("input", (event) => {
+    const currentConfirmPassword = document.getElementById("confirm-password");
+    if(password.value === currentConfirmPassword.value){
+        currentConfirmPassword.setCustomValidity("");
+    } else {
+        currentConfirmPassword.setCustomValidity("Passwords don't match");
     }
 });
